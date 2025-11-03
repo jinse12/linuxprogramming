e65f829bda3c3d9f137d41de05a568b538339d09
jinse12 <dtd1811@naver.com>
2025-11-03 23:18:22 +0900

    실습과제2

---
+++ chap7-1/work2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream> //입출력 헤더 포함
+#include "Complex.hpp" //Complex.hpp 헤더 포함
+#include "ComplexManager.hpp" //ComplexManager.hpp 헤더 포함
+using namespace std; //std:: 생략
+
+int main() {
+    Complex x(2, 3), y(-5, 10), sum; //복소수 객체 3개 생성
+    ComplexManager man;              //ComplexManager 객체 생성
+
+    //ComplexManager의 멤버함수를 사용하여 두 복소수를 더함
+    sum = man.ComplexAdd(x, y);
+
+    cout << "두 복소수의 합은 ";
+    sum.show(); //결과 출력
+
+    return 0; //실행 종료
+}
