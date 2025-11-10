b2dea71289cb97a6c8b12f329e17169b2a426898
jinse12 <dtd1811@naver.com>
2025-11-10 15:19:28 +0900

    실습과제1

--- chap7-4/work1/complex.cpp
+++ chap7-4/work1/complex.cpp
@@ -14,16 +14,18 @@ Complex::Complex(double r, double i) {
 Complex& Complex::operator++() {
     ++real;
     ++img;
-    return *this; //자기 자신을 참조로 반환
+    return *this; //자기 자신을 참조로 반환. 반환값을 참조로. 주소에 있는 값!이 리턴됨
+    //*this는 전위 증가로 real, img가 1씩 증가한 현재 객체 자체를 가리킴. 그 객체가 그대로 반환된다.
 }
+//리턴값은 일반적으로 상수취급
 
-//후위 감소 연산자
+//후위 감소 연산자. 후위는 상수이기 때문에 &를 안붙임
 //현재 값을 반환하고 나중에 감소
-Complex Complex::operator--(int) { //int는 구분자
-    Complex tmp = *this; //현재 상태 저장
+Complex Complex::operator--(int) { //함수의 형태를 구분하기 위해 임의의 정수인 int를 선언.
+    Complex tmp = *this; //증가 이전 객체 상태 저장
     real--; //실수부 감소
     img--;  //허수부 감소
-    return tmp; //감소 전 상태 반환
+    return tmp; //감소하기 이전의 객체 상태를 반환
 }
 
 //복소수 출력 함수
