e0a679022a70be35ac3753bc614cd6d5c566c73c
jinse12 <dtd1811@naver.com>
2025-11-10 20:32:09 +0900

    실습과제2

--- chap7-4/work2/complex.hpp
+++ chap7-4/work2/complex.hpp
@@ -13,12 +13,11 @@ public:
 
     //프렌드 연산자 함수 선언
     //Complex 클래스의 private 멤버(real, img)에도 접근 가능
-    friend Complex operator*(const Complex& a, const Complex& b); //곱셈
     friend Complex& operator++(Complex& a);       //전위 증가
     friend Complex operator--(Complex& a, int);   //후위 감소
 
     //출력 함수
     void show();
-};
+}
 
 #endif
