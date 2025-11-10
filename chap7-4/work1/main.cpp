4b37781b4001458723bb7a0f167907100809584e
jinse12 <dtd1811@naver.com>
2025-11-10 12:27:43 +0900

    실습과제1

--- chap7-4/work1/main.cpp
+++ chap7-4/work1/main.cpp
@@ -1,17 +1,21 @@
-#include <iostream>
-#include "complex.hpp"
-using namespace std;
+#include <iostream>     //입출력 사용을 위한 헤더 포함
+#include "complex.hpp"  //Complex 클래스 선언 포함
+using namespace std;    //std:: 생략
 
 int main() {
-    Complex x(2, 3);  // 생성자에서 "복소수2+3j생성" 출력
+    //Complex 객체 생성 시, 생성자에서 자동으로 메시지 출력됨
+    Complex x(2, 3);
 
+    //전위 증가 연산 (++x)
     ++x;
-    cout << "증가결과";
-    x.show();
+    cout << "증가결과"; //출력 문구
+    x.show();           //증가된 값 출력 (3+4j)
 
+    //후위 감소 연산 (x--)
     x--;
-    cout << "감소결과";
-    x.show();
+    cout << "감소결과"; //출력 문구
+    x.show();           //감소된 값 출력 (2+3j)
 
     return 0;
 }
+
