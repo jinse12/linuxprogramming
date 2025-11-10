d6779777d76173e8b0c67321e88334513c46bbd5
jinse12 <dtd1811@naver.com>
2025-11-10 14:15:52 +0900

    실습과제1

--- chap7-4/work1/complex.hpp
+++ chap7-4/work1/complex.hpp
@@ -12,7 +12,6 @@ public:
     Complex(double r = 0, double i = 0);
 
     //연산자 오버로딩 (멤버 함수로 구현)
-    Complex operator*(Complex a); //복소수 곱셈 연산자
     Complex& operator++();        //전위 증가 연산자
     Complex operator--(int);      //후위 감소 연산자
 
