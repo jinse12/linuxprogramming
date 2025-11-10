8b8a6f08a3f88d84092cf56157fbb0daabed4540
jinse12 <dtd1811@naver.com>
2025-11-10 13:11:16 +0900

    실습과제1

--- chap7-4/work1/complex.cpp
+++ chap7-4/work1/complex.cpp
@@ -6,18 +6,18 @@ using namespace std; //std:: 생략
 Complex::Complex(double r, double i) {
     real = r;
     img = i;
-    cout << "복소수" << real << "+" << img << "j생성" << endl;
+    cout << "복소수 " << real << " + " << img << "j 생성" << endl;
 }
 
 //복소수 곱셈 연산자 오버로딩
 Complex Complex::operator*(Complex a) {
     Complex tmp; //연산 결과를 저장할 임시 객체
     tmp.real = this->real * a.real - this->img * a.img; //실수부 계산
-    tmp.img = this->real * a.img + this->img * a.real; //허수부 계싼
+    tmp.img = this->real * a.img + this->img * a.real; //허수부 계산
     return tmp; //계산 결과 반환
 }
 
-//전위 증가 연산자 (++x)
+//전위 증가 연산자
 //실수부와 허수부를 각각 1씩 증가
 Complex& Complex::operator++() {
     ++real;
@@ -25,7 +25,7 @@ Complex& Complex::operator++() {
     return *this; //자기 자신을 참조로 반환
 }
 
-//후위 감소 연산자 (x--)
+//후위 감소 연산자
 //감소 전 값을 반환해야 하므로 복사본을 만들어 반환
 Complex Complex::operator--(int) {
     Complex tmp = *this; //현재 상태 저장
@@ -36,5 +36,5 @@ Complex Complex::operator--(int) {
 
 //복소수 출력 함수
 void Complex::show() {
-    cout << real << " + " << img << "j" << endl;
+    cout << real << " + " << img << "j " << endl;
 }
