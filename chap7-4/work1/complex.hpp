23d783c9596a70c25aff691a52ae6665bdf8260a
jinse12 <dtd1811@naver.com>
2025-11-10 13:10:43 +0900

    실습과제1

--- chap7-4/work1/complex.hpp
+++ chap7-4/work1/complex.hpp
@@ -1,22 +1,22 @@
 #ifndef COMPLEX_HPP //중복 포함 방지 
 #define COMPLEX_HPP
 
-//Complex 클래스 정의
+//Complex 클래스 선언
 class Complex {
 private:
-    double real;  // 실수부 변수
-    double img;   // 허수부 변수
+    double real;  //실수부 변수
+    double img;   //허수부 변수
 
 public:
-    // 생성자 (기본값을 0으로
+    //생성자 (기본값을 0으로
     Complex(double r = 0, double i = 0);
 
-    // 연산자 오버로딩 (멤버 함수로 구현)
-    Complex operator*(Complex a); // 복소수 곱셈 연산자
-    Complex& operator++();        // 전위 증가 연산자(++x)
-    Complex operator--(int);      // 후위 감소 연산자(x--)
+    //연산자 오버로딩 (멤버 함수로 구현)
+    Complex operator*(Complex a); //복소수 곱셈 연산자
+    Complex& operator++();        //전위 증가 연산자
+    Complex operator--(int);      //후위 감소 연산자
 
-    // 복소수 출력 함수
+    //복소수 출력 함수
     void show();
 };
 
