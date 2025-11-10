dd7c67f6b34ab387a331737dc27d91961ba1833c
jinse12 <dtd1811@naver.com>
2025-11-10 12:24:22 +0900

    실습과제1

--- chap7-4/work1/complex.cpp
+++ chap7-4/work1/complex.cpp
@@ -1,38 +1,40 @@
-#include <iostream>
-#include "complex.hpp"
-using namespace std;
+#include <iostream> //표준 입출력 사용을 위한 헤더 포함
+#include "complex.hpp" //complex 클래스 헤더 포함
+using namespace std; //std:: 생략
 
-// 생성자
+// 생성자 구현
 Complex::Complex(double r, double i) {
-    this->real = r;
-    this->img = i;
+    real = r;
+    img = i;
     cout << "복소수" << real << "+" << img << "j생성" << endl;
 }
 
-// 곱셈 연산자
+// 복소수 곱셈 연산자 오버로딩
 Complex Complex::operator*(Complex a) {
-    Complex tmp;
-    tmp.real = this->real * a.real - this->img * a.img;
-    tmp.img = this->real * a.img + this->img * a.real;
-    return tmp;
+    Complex tmp; //연산 결과를 저장할 임시 객체
+    tmp.real = this->real * a.real - this->img * a.img; //실수부 계산
+    tmp.img = this->real * a.img + this->img * a.real; //허수부 계싼
+    return tmp; //계산 결과 반환
 }
 
-// 전위 증가 연산자 (++x)
+//전위 증가 연산자 (++x)
+//실수부와 허수부를 각각 1씩 증가
 Complex& Complex::operator++() {
     ++real;
     ++img;
-    return *this;
+    return *this; //자기 자신을 참조로 반환
 }
 
-// 후위 감소 연산자 (x--)
+//후위 감소 연산자 (x--)
+//감소 전 값을 반환해야 하므로 복사본을 만들어 반환
 Complex Complex::operator--(int) {
-    Complex tmp = *this;
-    real--;
-    img--;
-    return tmp;
+    Complex tmp = *this; //현재 상태 저장
+    real--; //실수부 감소
+    img--;  //허수부 감소
+    return tmp; //변경 전 상태 반환
 }
 
-// 출력 함수
+//복소수 출력 함수
 void Complex::show() {
-    cout << real << "+" << img << "j" << endl;
+    cout << real << " + " << img << "j" << endl;
 }
