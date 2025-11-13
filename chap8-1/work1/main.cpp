6c914b6257b16b93ee530ca33578e7b86f8d96cf
jinse12 <dtd1811@naver.com>
2025-11-13 16:07:21 +0900

    실습과제1

--- chap8-1/work1/main.cpp
+++ chap8-1/work1/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>   //입출력을 사용하기 위해 포함
 #include "shape.hpp"  //Shape, Circle, Rect, Triangle 클래스의 선언을 포함
-using namespace std;  //std: 생략
+using namespace std;  //std:: 생략
 
 int main() {
     Circle x;      //원 객체
     Rect y;        //직사각형 객체
     Triangle z;    //삼각형 객체
 
-    x.setCircle(0, 0, 2);      //(0,0), 반지름 2
-    y.setRect(1, 1, 5, 10);    //(1,1), 폭 5, 높이 10
-    z.setTriangle(2, 2, 3, 4); //(2,2), 밑변 3, 높이 4
+    x.setCircle(0, 0, 2);       //외부에서는 set으로 xy값을 넣고, 내부에서는 get로 값을 읽는다.
+    y.setRect(1, 1, 5, 10);
+    z.setTriangle(2, 2, 3, 4);
 
     x.show(); //원 정보 출력
     y.show(); //직사각형 정보 출력
