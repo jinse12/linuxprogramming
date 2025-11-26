ca119f81d92ee730ac0df4d0f291efa24b158db6
jinse12 <dtd1811@naver.com>
2025-11-26 16:57:13 +0900

    실습과제2

--- chap10-3/work2/vector.cpp
+++ chap10-3/work2/vector.cpp
@@ -1,24 +1,30 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+//******************************************************
+// 제목: 실습과제 2
+// 날짜: 2025년 11월 26일
+// 작성자: 2001178 김진세
+//******************************************************
+
+include <iostream> //입출력 사용을 위해 포함
+#include <vector>   //vector 포함
+using namespace std; //std:: 생략을 위해 사용
 
 int main() {
-    vector<int> numbers;  // 정수를 저장할 벡터
-    int num;
+    vector<int> numbers;  //정수를 저장할 벡터
+    int num; //
 
-    cout << "정수 4개를 입력하시오: ";
-    
-    // 벡터에 4개의 정수 입력
+    cout << "정수 4개를 입력하시오: "; //입력 메시지
+
+    //벡터에 4개의 정수 입력
     for (int i = 0; i < 4; ++i) {
         cin >> num;
-        numbers.push_back(num); // 입력 값을 벡터에 추가
+        numbers.push_back(num); //입력 값을 벡터에 추가
     }
 
-    // 최대값과 최소값 초기화
+    //최대값과 최소값 초기화
     int max_val = numbers[0];
     int min_val = numbers[0];
 
-    // 벡터를 순회하며 최대값과 최소값 계산
+    //벡터를 순회하며 최대값과 최소값 계산
     for (int i = 1; i < numbers.size(); ++i) {
         if (numbers[i] > max_val) max_val = numbers[i];
         if (numbers[i] < min_val) min_val = numbers[i];
