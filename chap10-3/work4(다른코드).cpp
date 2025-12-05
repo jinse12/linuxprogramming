1943a5ea650ffc6e1e789d8981d6abbcff1d0292
jinse12 <dtd1811@naver.com>
2025-12-05 19:13:26 +0900

    Create work4(다른코드).cpp

---
+++ chap10-3/work4(다른코드).cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> number;
+    vector<int>::iterator it;
+    int num;
+
+    cout << "ㅅㅈ ㅇㄹ: ";
+    for (int i = 0; i < 5; i++) {
+        cin >> num;
+        number.push_back(num);
+    }
+
+    cout << "ㅊㄱ ㅂㅌ ㅇㅅ: ";
+    for (it = number.begin(); it < number.end(); it++) {
+        cout << *it << ' ';
+    }
+    cout << endl;
+
+    for (it = number.begin(); it != number.end(); ) {
+        if (*it < 0) {
+            it = number.erase(it);
+        }
+        else {
+            it++;
+        }
+    }
+
+    cout << "ㅇㅅ ㅅㅈ ㅎ: ";
+    for (it = number.begin(); it < number.end(); it++) {
+        cout << *it << ' ';
+    }
+    cout << endl;
+
+    double sum = 0.0;
+    for (int i = 0; i < number.size(); i++) {
+        sum += number[i];
+    }
+    double avg = sum / number.size();
+
+    cout << "평균값 : " << avg << endl;
+
+    return 0;
+}
